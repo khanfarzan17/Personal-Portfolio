6e93086212ae5e7b2d77d863399f7b0b08e8fdb8
khanfarzan17 <khanfarzan200@gmail.com>
2024-08-16 11:01:58 +0530

    NavBar Completed With Responsiveness

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.Navbar}>
+      <a href="/" className={styles.title}>
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen ? "assests/nav/closeIcon.png" : "assests/nav/menuIcon.png"
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
