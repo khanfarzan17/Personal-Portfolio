3fb62f17bfbd5488b377446c88da9be005417f87
khanfarzan17 <khanfarzan200@gmail.com>
2024-08-16 21:14:19 +0530

    Experinece and Skill Page Compelete

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import styles from "./App.module.css";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
+import Experience from "./Components/Experience";
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   );
 };
--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -1,10 +1,44 @@
 import React from "react";
-
+import skills from "../Data/Skills.json";
+import history from "../Data/History.json";
+import styles from "./Experience.module.css";
 const Experience = () => {
   return (
-    <div>
-      <h1>Expereince</h1>
-    </div>
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Experience</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {skills.map((skill, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={skill.imgesrc} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+        <ul className={styles.history}>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id} className={styles.historyItem}>
+                <img
+                  src={historyItem.imgesrc}
+                  alt={historyItem.Oraganization}
+                />
+                <div className={styles.historyItemDetails}>
+                  <h3>{` ${historyItem.role}, ${historyItem.Oraganization}`}</h3>
+
+                  <p>{`  ${historyItem.startDate} to ${historyItem.endDate}`}</p>
+                  <p>Location {historyItem.Location} </p>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
   );
 };
 
